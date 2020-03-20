13ba03ccffc927d4908894e8564349a46b59d47c
rmurgu <no-reply>
2020-03-20 16:30:41 +0200

    Add web app module dependent on other two modules

---
+++ web-app/src/main/java/controllers/UserController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import com.example.User;
+import com.example.UserCommand;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+        // fake impl
+        return new User();
+    }
+
+}
